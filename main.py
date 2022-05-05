5b89f86ff02d63d754635cc6323209d457e6ca22
alimoradynezhad <asmnbala@gmail.com>
2022-05-05 13:03:50 +0430

    change more than char

---
+++ main.py
@@ -0,0 +1,29 @@
+import os  # library
+from argparse import ArgumentParser
+
+char = int(input("please inter number for change object"))
+
+parser = ArgumentParser()
+parser.add_argument("--labels_path", required=True, help="Path to labels' directory")
+args = parser.parse_args()
+
+labels_path = args.labels_path
+dir_list = os.listdir(labels_path)
+
+print("-----------------------------------------")
+print("Files and directories in '", labels_path, "' :")
+print(dir_list)
+
+
+
+for file_name in dir_list:
+    txt_path = os.path.join(labels_path, file_name)
+    print(f"txt_path: {txt_path}")
+
+    with open(txt_path, mode="r") as f:
+        first_line = f.read()
+
+    modified_label = first_line.replace('-1', '0', char)
+    with open(txt_path, mode="w") as f:
+        f.write(modified_label)
+print(f"Successfully processed labels in {labels_path} directory")
